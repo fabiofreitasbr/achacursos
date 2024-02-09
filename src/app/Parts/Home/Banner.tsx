21b5065f2a69bb20e97645fa72ac326a3e3e7b61
Fábio Freitas <fabiofreitassilvacontato@gmail.com>
2024-02-09 12:10:23 -0300

    Home Banner

--- src/app/Parts/Home/Banner.tsx
+++ src/app/Parts/Home/Banner.tsx
@@ -4,7 +4,7 @@ function Banner() {
             <div className="container mx-auto px-4">
                 <div className="swiper rounded-xl md:rounded-2xl overflow-hidden">
                     <div className="swiper-wrapper">
-                        <div className="swiper-slide"><a href="" className="w-full" target="_blank"><img src="img/banner.png" alt="" className="w-full" /></a></div>
+                        <div className="swiper-slide"><a href="" className="w-full" target="_blank"><img src="img/banner.jpg" alt="" className="w-full" /></a></div>
                     </div>
                     <div className="swiper-pagination"></div>
                     <div className="swiper-button-prev after:text-red-700 after:!text-2xl after:md:!text-4xl"></div>
