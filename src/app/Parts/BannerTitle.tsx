039e5a2af74ce0afd62dd534ab1f26217932a8f8
Fábio Freitas <fabiofreitassilvacontato@gmail.com>
2024-04-17 13:34:06 -0300

    Banner

--- src/app/Parts/BannerTitle.tsx
+++ src/app/Parts/BannerTitle.tsx
@@ -8,7 +8,7 @@ const BannerTitle = (Props: bannerInfo) => {
         <div className="banner bg-gray-100 bg-cover bg-center min-h-[6rem] sm:min-h-[8rem] md:min-h-[8rem] lg:min-h-[10rem] xl:min-h-[12rem] 2xl:min-h-[16rem] py-8 flex justify-center items-center" style={{backgroundImage: 'url(img/background-banner.jpg)'}}>
             <div
                 className="text-sm md:text-xl text-black rounded-xl flex align-items justify-center h-full flex-col max-w-xl text-center">
-                <h2 className="text-white text-3xl md:text-3xl lg:text-4xl uppercase leading-tight font-bold leading-relaxed my-2 md:my-2 ">
+                <h2 className="text-white text-3xl md:text-3xl lg:text-4xl uppercase leading-tight font-bold my-2 md:my-2 ">
                     <span className="text-blue-500">{Props.Title}</span>
                 </h2>
             </div>
