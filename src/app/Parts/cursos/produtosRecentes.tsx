dda0a4fb6d77d1e219cb5eb38d00573bdfec6941
Fábio Freitas <fabiofreitassilvacontato@gmail.com>
2024-04-18 00:56:42 -0300

    Conteúdo

--- src/app/Parts/cursos/produtosRecentes.tsx
+++ src/app/Parts/cursos/produtosRecentes.tsx
@@ -1,15 +1,48 @@
+import axios from "axios";
+import Image from "next/image";
+import Link from "next/link";
+import { Suspense } from "react";
+import { LoadingRecentes } from "../Utils/loading";
 
-import ProductRecents from '@/app/Components/ProductRecents';
+export async function ListBlogRecentes() {
+    const { data } = await axios.get("http://localhost:3001/cursos/recentes");
+    return (
+        <div>
+            {
+                data.map((artigoCurrent: any) => (
+                    <div key={artigoCurrent.slug} className="w-full py-2">
+                        <Link href={"/cursos/" + artigoCurrent.slug}>
+                            <div className="rounded-xl overflow-hidden shadow-lg shadow-gray-300 flex">
+                                <div className="w-1/3">
+                                    <Image className="w-full h-full overflow-hidden object-cover" alt={artigoCurrent.title} width={103} height={108} src={"/img/" + artigoCurrent.image} />
+                                </div>
+                                <div className="w-2/3 px-4 pt-4 pb-1 uppercase">
+                                    <h4 className="text-xs text-orange">
+                                        {artigoCurrent.title}
+                                    </h4>
+                                    <h3 className="text-base text-blue-500 hover:text-red-500 font-medium">{artigoCurrent.subtitle}</h3>
+                                    <div className="text-blue-500 text-right py-2 text-xs font-bold">
+                                        VER MAIS
+                                    </div>
+                                </div>
+                            </div>
+                        </Link>
+                    </div>
+                ))
+            }
+        </div>
+    )
+}
 
 export default function ProdutosRecentes() {
     return (
-        <div className="mb-4 md:mb-10 hidden md:block">
-            <h3 className="text-xl my-2 font-medium text-blue-500 uppercase">
-                Mais Recentes
-            </h3>
-            <ProductRecents />
-            <ProductRecents />
-            <ProductRecents />
-        </div>
+        <>
+            <div className="mb-4 md:mb-10 hidden md:block">
+                <h3 className="text-xl my-2 font-medium text-blue-500 uppercase">Mais Recentes</h3>
+                <Suspense fallback={<LoadingRecentes />}>
+                    <ListBlogRecentes />
+                </Suspense>
+            </div>
+        </>
     )
 }
--- src/app/Parts/cursos/produtosTags.tsx
+++ src/app/Parts/cursos/produtosTags.tsx
@@ -1,22 +1,38 @@
-export default function ProdutosTags() {
+import axios from "axios";
+import Link, { LinkProps } from "next/link";
+import { AnchorHTMLAttributes } from "react";
+
+interface TagSingleProps extends LinkProps {
+    children: String;
+}
+interface tagInfo {
+    id: string;
+    title: string;
+    slug: string;
+}
+
+function TagSingle({children, href}:TagSingleProps ) {
+    return (
+        <div>
+            <Link href={href}>
+                <span className="inline-block my-1 mx-1 md:mx-0 md:my-2 py-2 px-2 md:px-4 text-xs md:text-lg border md:border-2 border-blue-500 hover:bg-blue-500 text-blue-500 hover:text-white transition-all font-bold">
+                    {children}
+                </span>
+            </Link>
+        </div>
+    )
+}
+export default async function BlogTags() {
+    const tags = await axios.get('http://localhost:3001/tags/find');
     return (
         <div className="mb-2 md:mb-10">
             <h3 className="text-xl my-2 font-medium text-blue-500 uppercase">Tags</h3>
             <div className="my-2 flex flex-wrap md:block">
-                <div>
-                    <a href="https://www.redeconomia.com.br/dicas-receitas/bebidas/">
-                        <span className="inline-block my-1 mx-1 md:mx-0 md:my-2 py-2 px-2 md:px-4 text-xs md:text-lg text-blue-500 font-bold uppercase border md:border-2 border-blue-500">
-                            Bebidas
-                        </span>
-                    </a>
-                </div>
-                <div>
-                    <a href="https://www.redeconomia.com.br/dicas-receitas/comidas/">
-                        <span className="inline-block my-1 mx-1 md:mx-0 md:my-2 py-2 px-2 md:px-4 text-xs md:text-lg text-blue-500 font-bold uppercase border md:border-2 border-blue-500">
-                            Comidas
-                        </span>
-                    </a>
-                </div>
+                {
+                    tags.data.map((tagSingle:tagInfo) => (
+                        <TagSingle key={tagSingle.id} href={"/tag/" + tagSingle.slug}>{tagSingle.title}</TagSingle>
+                    ))
+                }
             </div>
         </div>
     )
