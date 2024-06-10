f70d630f331d0ee009c9d44b5676f0d867ee5ec0
Fábio Freitas <fabiofreitassilvacontato@gmail.com>
2024-06-10 18:13:27 -0300

    Páginas

--- src/app/Parts/Components/ProductHighlight.tsx
+++ src/app/Parts/Components/ProductHighlight.tsx
@@ -1,8 +1,11 @@
+import Image from "next/image"
+
 export default function ProductHighlight(props: any) {
     return (
         <a href={"/curso/" + props.content.slug}>
-            <div className="bg-black bg-cover bg-center h-80 flex items-end" style={{ backgroundImage: "url(/img/" + props.content.image + ");" }}>
-                <div className=" text-white px-4 py-3 font-bold text-xl">
+            <div className="h-80 flex items-end relative group overflow-hidden">
+                <Image src={"/img/" + props.content.image} width={368} height={320} className="absolute top-0 w-full h-full object-cover z-10 group-hover:scale-110 transition" alt="" />
+                <div className="z-20 text-white px-4 py-3 font-bold text-xl">
                     <h4>{props.content.title}</h4>
                     <h5>R$ {props.content.price}</h5>
                 </div>
