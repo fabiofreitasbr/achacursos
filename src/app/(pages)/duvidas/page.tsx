467d6a65e812ae6e51a16f2c4ae2b0a65f5b54c9
Fábio Freitas <fabiofreitassilvacontato@gmail.com>
2024-06-13 10:32:14 -0300

    Curso

--- src/app/(pages)/duvidas/page.tsx
+++ src/app/(pages)/duvidas/page.tsx
@@ -3,6 +3,8 @@ import Duvidas from '@/app/Parts/Components/Duvidas'
 export default function Page() {
     return (
         <>
+            <h3 className="my-2 font-medium text-2xl font-open uppercase text-blue-500">Dúvidas Frequentes</h3>
+            <h4 className="font-reading text-lg mb-4">Tire todas as suas dúvidas abaixo!</h4>
             <Duvidas />
         </>
     )
--- src/app/Parts/Components/Duvidas.tsx
+++ src/app/Parts/Components/Duvidas.tsx
@@ -1,49 +1,61 @@
 'use client'
 import { Accordion } from "flowbite-react"
 
+const ListDuvidas = [
+    {
+        title: 'Como faço para comprar um curso?',
+        slug: 'compra',
+        content: 'Você entra na página do curso e clica em comprar ou em conhecer o curso, então é redirecionado para a instituição de ensino ou página de curso para comprar o curso que deseja! A partir de então a instituição passar a ser 100% responsável pela entrega do material e conteúdo do curso.'
+    },
+    {
+        title: 'Como são as aulas e cursos?',
+        slug: 'aulas',
+        content: 'As aulas dos cursos online são vídeos gravados que você pode acessar através de plataforma e ver as aulas quando quiser! Os e-books são enviados em PDF para leitura. Os cursos presenciais e faculdades precisam ser verificados a disponibilidade na sua região ou se existe EAD para o caso das faculdades.'
+    },
+    {
+        title: 'Como são as aulas e cursos?',
+        slug: 'aulas',
+        content: 'As aulas dos cursos online são vídeos gravados que você pode acessar através de plataforma e ver as aulas quando quiser! Os e-books são enviados em PDF para leitura. Os cursos presenciais e faculdades precisam ser verificados a disponibilidade na sua região ou se existe EAD para o caso das faculdades.'
+    },
+    {
+        title: 'Os cursos da plataforma são atualizados?',
+        slug: 'atualizacoes',
+        content: 'O curso contém todas as aulas de tecnologias mais modernas usadas no mercados e exemplos reais de uso dessas tecnologias.'
+    },
+    {
+        title: 'E se eu tiver dúvidas e dificuldades?',
+        slug: 'duvidas',
+        content: 'Em cada curso normalmente tem uma área de dúvidas, comunidade e suporte. Importante conferir com os responsáveis do curso.'
+    },
+    {
+        title: 'Como funcionam garantias?',
+        slug: 'garantias',
+        content: 'Cada curso tem um prazo mínimo de solicitar a garantia, a quantidade mínima é de 7 dias. Confira na página do curso escolhido a garantia.'
+    },
+    {
+        title: 'Consigo acesso a bolsas e descontos?',
+        slug: 'descontos',
+        content: 'O propósito real do Acha Cursos é encontrar as melhores ofertas de cursos.'
+    },
+]
+
 const Duvidas = () => {
     return(
         <Accordion collapseAll className='border-none'>
-            <Accordion.Panel>
-                <Accordion.Title className="p-4 bg-gray-100 text-black focus:bg-red-500 focus:text-white hover:bg-gray-200 rounded-md my-2 text-xs md:text-sm lg:text-base">
-                Como faço para comprar um curso?
-                </Accordion.Title>
-                <Accordion.Content className="p-4">
-                    <p className="mb-2 text-gray-500 text-xs md:text-sm lg:text-base">
-                        Você entra na página do curso e clica em comprar ou em conhecer o curso, então é redirecionado para a instituição de ensino ou página de curso para comprar o curso que deseja!
-                    </p>
-                </Accordion.Content>
-            </Accordion.Panel>
-            <Accordion.Panel>
-                <Accordion.Title className="p-4 bg-gray-100 text-black focus:bg-red-500 focus:text-white hover:bg-gray-200 rounded-md my-2 text-xs md:text-sm lg:text-base">
-                    Como são as aulas?
-                </Accordion.Title>
-                <Accordion.Content className="p-4">
-                    <p className="mb-2 text-gray-500 text-xs md:text-sm lg:text-base">
-                        As aulas são vídeos gravados que você pode acessar através de plataforma e ver as aulas quando quiser!
-                    </p>
-                </Accordion.Content>
-            </Accordion.Panel>
-            <Accordion.Panel>
-                <Accordion.Title className="p-4 bg-gray-100 text-black focus:bg-red-500 focus:text-white hover:bg-gray-200 rounded-md my-2 text-xs md:text-sm lg:text-base">
-                    O curso é bom ou atualizado?
-                </Accordion.Title>
-                <Accordion.Content className="p-4">
-                    <p className="mb-2 text-gray-500 text-xs md:text-sm lg:text-base">
-                        O curso contém todas as aulas de tecnologias mais modernas usadas no mercados e exemplos reais de uso dessas tecnologias.
-                    </p>
-                </Accordion.Content>
-            </Accordion.Panel>
-            <Accordion.Panel>
-                <Accordion.Title className="p-4 bg-gray-100 text-black focus:bg-red-500 focus:text-white hover:bg-gray-200 rounded-md my-2 text-xs md:text-sm lg:text-base">
-                    E se eu tiver alguma dúvida?
-                </Accordion.Title>
-                <Accordion.Content className="p-4">
-                    <p className="mb-2 text-gray-500 text-xs md:text-sm lg:text-base">
-                        Em cada aula você possui uma área para enviar suas dúvidas, receber suporte e respostas sobre o curso.
-                    </p>
-                </Accordion.Content>
-            </Accordion.Panel>
+            {
+                ListDuvidas.map((singleDuvidas) => (
+                    <Accordion.Panel key={singleDuvidas.slug}>
+                        <Accordion.Title className="p-4 bg-gray-100 text-black focus:bg-red-500 focus:text-white hover:bg-gray-200 rounded-md my-2 text-sm lg:text-base">
+                            {singleDuvidas.title}
+                        </Accordion.Title>
+                        <Accordion.Content className="p-4">
+                            <p className="mb-2 text-gray-500 text-sm lg:text-base">
+                                {singleDuvidas.content}
+                            </p>
+                        </Accordion.Content>
+                    </Accordion.Panel>
+                ))
+            }
         </Accordion>
     )
 };
