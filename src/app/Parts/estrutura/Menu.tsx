e5ff8f64360663bcfa596c4c11641ab4e48caa41
Fábio Freitas <fabiofreitassilvacontato@gmail.com>
2024-06-13 07:50:48 -0300

    Page

--- src/app/Parts/estrutura/Menu.tsx
+++ src/app/Parts/estrutura/Menu.tsx
@@ -104,7 +104,7 @@ const Menu = () => {
                     </div>
                 </div>
             </header>
-            <div onClick={menuBurger} className={"z-10 menu bg-blue-600 md:bg-blue-500 text-white w-full md:w-auto h-screen md:h-auto fixed md:relative md:right-0 top-0 px-2 md:px-6 pt-20 md:pt-0 transition-all duration-300 ease-in-out  " + menuNavigation}>
+            <div className={"z-10 menu bg-blue-600 md:bg-blue-500 text-white w-full md:w-auto h-screen md:h-auto fixed md:relative md:right-0 top-0 px-2 md:px-6 pt-20 md:pt-0 transition-all duration-300 ease-in-out  " + menuNavigation}>
                 <nav className="">
                     <div className="container mx-auto px-0 md:px-4 uppercase">
                         <ul className="z-10 top-menu flex flex-col md:flex-row justify-between md:items-center text-base lg:text-lg" id="top-menu">
