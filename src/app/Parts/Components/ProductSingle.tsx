68e7a6f4ea66af4545fcab9582bcfe308c563711
Fábio Freitas <fabiofreitassilvacontato@gmail.com>
2025-05-27 01:08:15 -0300

    atualizado

--- src/app/Parts/Components/ProductSingle.tsx
+++ src/app/Parts/Components/ProductSingle.tsx
@@ -3,31 +3,22 @@ import { Params } from '../Types/searchParams';
 import NotFoundComponent from '../estrutura/notFoundComponent';
 import ProductSingleContent from './ProductSingleContent';
 
-export default async function ProductSingle({ params }: { params: Params }) {
-    if (!params || !params.slug) {
-        return <NotFoundComponent />;
-    }
-    const slug = params.slug;
+export default async function ProductSingle({ params }: { params: Promise<Params> }) {
+    const { slug } = await params;
+    const res = await axios.get(`${process.env.local_api}cursos/single`, {
+        params: { slug: slug || "" },
+    });
+    const data = res.data;
 
-    try {
-        const res = await axios.get(`${process.env.local_api}cursos/single`, {
-            params: { slug: slug || "" },
-        });
-        const data = res.data;
-
-        return (
-            <>
-                {data ? (
-                    <div className="container mx-auto px-4">
-                        <ProductSingleContent data={data} />
-                    </div>
-                ) : (
-                    <NotFoundComponent />
-                )}
-            </>
-        );
-    } catch (error) {
-        console.error("Erro ao buscar curso:", error);
-        return <NotFoundComponent />;
-    }
+    return (
+        <>
+            {data ? (
+                <div className="container mx-auto px-4">
+                    <ProductSingleContent data={data} />
+                </div>
+            ) : (
+                <NotFoundComponent />
+            )}
+        </>
+    );
 }
